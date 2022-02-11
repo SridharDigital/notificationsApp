956316cbe8e7612152463986ef80557c7d50a260
Sreedharan Namboodiri C <sridhardigital22@gmail.com>
2022-02-11 08:16:22 +0000

    Final Version

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
-const Notification = props => {
-  //  Write your code here.
-}
+const Notification = (props) => {
+  const { boxText, boxClass, boxImage } = props;
+  return (
+    <div className={boxClass + " notification-box"}>
+      <img src={boxImage} className="icon" />
+      <p className="message">{boxText}</p>
+    </div>
+  );
+};
 
 const element = (
-  //  Write your code here.   
-)
+  <div className="bg-container">
+    <div className="content-container">
+      <h1 className="main-heading">Notifications</h1>
+      <Notification
+        boxText="Information Message"
+        boxClass="information"
+        boxImage="https://assets.ccbp.in/frontend/react-js/primary-icon-img.png"
+      />
 
-ReactDOM.render(element, document.getElementById('root'))
+      <Notification
+        boxText="Success Message"
+        boxClass="success"
+        boxImage="https://assets.ccbp.in/frontend/react-js/success-icon-img.png"
+      />
+
+      <Notification
+        boxText="Warning Message"
+        boxClass="warning"
+        boxImage="https://assets.ccbp.in/frontend/react-js/warning-icon-img.png"
+      />
+
+      <Notification
+        boxText="Danger Message"
+        boxClass="danger"
+        boxImage="https://assets.ccbp.in/frontend/react-js/danger-icon-img.png"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
